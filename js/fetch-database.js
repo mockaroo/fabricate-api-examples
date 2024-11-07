eeae60771f483a715741a2bc4dfbb93b41410085
Mark Brocato <mvbrocato@gmail.com>
2024-11-08 00:30:44 +0200

    formatting

--- js/fetch-database.js
+++ js/fetch-database.js
@@ -8,10 +8,7 @@ const DATABASE = process.env.DATABASE;
 async function main() {
   const response = await got(`https://fabricate.mockaroo.com/api/v1/databases/${DATABASE}`, {
     responseType: "json",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.FABRICATE_API_KEY}`,
-    },
+    headers: { Authorization: `Bearer ${process.env.FABRICATE_API_KEY}` },
   });
 
   console.log(response.body);
--- js/list-databases.js
+++ js/list-databases.js
@@ -4,15 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-  const response = await got(
-    "https://fabricate.mockaroo.com/api/v1/databases",
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.FABRICATE_API_KEY}`,
-      },
-      responseType: "json",
-    }
-  );
+  const response = await got("https://fabricate.mockaroo.com/api/v1/databases", {
+    responseType: "json",
+    headers: { Authorization: `Bearer ${process.env.FABRICATE_API_KEY}` },
+  });
 
   console.log(response.body);
 }
